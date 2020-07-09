4fce22f1907e1adff9b73f07ca67b8371f7b8b59
anshbhatt <52775304+anshbhatt@users.noreply.github.com>
2020-07-09 13:50:39 +0530

    Add files via upload

---
+++ accounts/decorators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import PermissionDenied
+
+def student_required(function):
+    def wrap(request):
+        if request.user.is_student == True: #checking if the user is a student
+            return function(request)
+        else:
+            return PermissionDenied         #Direct function which can be included. gives out an exception
+    return wrap
+    
+def teacher_required(function):
+    def wrap(request):
+        if request.user.is_teacher == True:
+            return function(request)
+        else:
+            return PermissionDenied
+    return wrap
