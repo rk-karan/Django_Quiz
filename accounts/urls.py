8fc40007cba3862e06feb17f41c0e02b9ad5a563
anshbhatt <52775304+anshbhatt@users.noreply.github.com>
2020-07-11 14:00:57 +0530

    Adding Questions Enabled

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,7 +19,13 @@ urlpatterns = [
 
 #once student registration is successful, take to student home
     path('student_home', views.student_home, name='student_home'),
-
+    
 #for creating quizzes by teachers
     path('create_quiz', views.create, name='create_quiz'),
+
+#for viewing quiz after it is created
+    path('quiz_view/<int:pk>', views.quiz_view, name='quiz_view'),
+    
+#for adding questions
+    path('add_questions/<int:pk>', views.add_questions, name='add_question'),
 ]
