399e48b7ac78efca54916f9222c10b26d0edd79c
anshbhatt <52775304+anshbhatt@users.noreply.github.com>
2020-07-11 14:01:45 +0530

    Adding Questions Enabled

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,17 +1,33 @@
 from django.core.exceptions import PermissionDenied
 
 def student_required(function):
-    def wrap(request):
+    def wrap(request, pk):
         if request.user.is_authenticated == True and request.user.is_student == True: #checking if the user is a student
-            return function(request)
+            return function(request, pk)
         else:
             raise PermissionDenied         #Direct function which can be included. gives out an exception
     return wrap
-
+    
 def teacher_required(function):
+    def wrap(request, pk):
+        if request.user.is_authenticated == True and request.user.is_teacher == True:
+            return function(request, pk)
+        else:
+            raise PermissionDenied
+    return wrap
+    
+def teacher_login(function):
     def wrap(request):
         if request.user.is_authenticated == True and request.user.is_teacher == True:
             return function(request)
         else:
             raise PermissionDenied
+    return wrap
+    
+def student_login(function):
+    def wrap(request):
+        if request.user.is_authenticated == True and request.user.is_student == True: #checking if the user is a student
+            return function(request)
+        else:
+            raise PermissionDenied         #Direct function which can be included. gives out an exception
     return wrap
