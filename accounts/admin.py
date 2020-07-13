0dfe7f1cf46cc1c30c9a004c6e4101e810a8b728
anshbhatt <52775304+anshbhatt@users.noreply.github.com>
2020-07-13 17:18:46 +0530

    Added a new model Question_info to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, student, teacher, Quiz, questions, answers
+from .models import User, student, teacher, Quiz, questions, answers, question_info
 
 admin.site.register(User)
 admin.site.register(student)
@@ -7,3 +7,4 @@ admin.site.register(teacher)
 admin.site.register(Quiz)
 admin.site.register(questions)
 admin.site.register(answers)
+admin.site.register(question_info)
